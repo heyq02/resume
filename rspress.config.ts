719fcfe78fd2a0871c9336c913b63a609d165d46
heyongqi10 <heyongqi10@jd.com>
2026-09-15 23:18:46 +0800

    fix(配置): 修正网站图标文件格式为jpg

--- rspress.config.ts
+++ rspress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   siteOrigin: 'https://heyq02.github.io',
   title: '贺永琪 - 简历',
   description: '贺永琪的个人简历 - 软件开发工程师',
-  icon: '/avator.png',
+  icon: '/avator.jpg',
   lang: 'zh',
   themeConfig: {
     socialLinks: [
