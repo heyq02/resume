9333ca24925986969a17422ba01a570e1b48e62b
heyongqi10 <heyongqi10@jd.com>
2026-09-15 21:53:47 +0800

    chore(config): 更改站点基础路径为resume

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@rspress/core';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: '/base/',
+  base: '/resume/',
   siteOrigin: 'https://heyq02.github.io',
   title: 'My Site',
   description: '',
